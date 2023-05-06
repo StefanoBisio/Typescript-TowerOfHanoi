c8089148efc77ce35e438bc4a1e576a53eb08dec
Stefano Bisio <s.b.milano@gmail.com>
2023-05-06 12:26:11 +0100

    add comments and styling fix to Disk.tsx

--- src/components/Disk/Disk.tsx
+++ src/components/Disk/Disk.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDrag } from 'react-dnd';
-import './Disk.module.css';
+import styles from './Disk.module.css';
+
 
 interface DiskProps {
   diskId: number;
@@ -8,25 +9,31 @@ interface DiskProps {
 }
 
 export const Disk: React.FC<DiskProps> = ({ diskId, towerIndex }) => {
+  // Use the useDrag hook from react-dnd to enable dragging of the disk
   const [{ isDragging }, drag] = useDrag(() => ({
+    // Set the type of the draggable item to 'disk'
     type: 'disk',
+    // Pass the diskId and towerIndex as the item data
     item: { id: diskId, source: towerIndex },
+    // Collect the isDragging property from the monitor
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
   }));
 
-  const diskWidth = 40 + diskId * 10;
-  const diskHeight = 20;
+  // Calculate the width and height of the disk based on its size
+  const diskWidth = 40 + diskId * 20;
 
   return (
     <div
+      // Attach the drag ref to enable dragging
       ref={drag}
-      className="disk"
+      className={styles.disk}
       style={{
         width: `${diskWidth}px`,
-        height: `${diskHeight}px`,
+        // Set the background color based on the disk size
         backgroundColor: `hsl(${diskId * 45}, 80%, 50%)`,
+        // Set the opacity to 0.5 when dragging, otherwise 1
         opacity: isDragging ? 0.5 : 1,
       }}
     ></div>
